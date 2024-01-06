dadcb56974a1575a7fc46b123329e457a81461e4
Eric Renfro <psi-jack@linux-help.org>
2024-01-05 22:12:36 -0500

    Added xseed to qbittorrent pod

---
+++ kubernetes/apps/downloads/qbittorrent/app/resources/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  delay: 20,
+  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local",
+  torznab: [
+    `http://prowlarr.downloads.svc.cluster.local/5/api?apikey=${process.env.PROWLARR_API_KEY}`, // ipt
+    `http://prowlarr.downloads.svc.cluster.local/39/api?apikey=${process.env.PROWLARR_API_KEY}`,  // fl
+    `http://prowlarr.downloads.svc.cluster.local/40/api?apikey=${process.env.PROWLARR_API_KEY}`, // ant
+    `http://prowlarr.downloads.svc.cluster.local/41/api?apikey=${process.env.PROWLARR_API_KEY}`, // ar
+    `http://prowlarr.downloads.svc.cluster.local/42/api?apikey=${process.env.PROWLARR_API_KEY}`, // tl
+  ],
+  port: process.env.CROSSSEED_PORT || 80,
+  apiAuth: false,
+  action: "inject",
+  includeEpisodes: false,
+  includeSingleEpisodes: true,
+  includeNonVideos: true,
+  duplicateCategories: true,
+  matchMode: "safe",
+  skipRecheck: true,
+  linkType: "hardlink",
+  linkDir: "/media/downloads/torrent/complete/cross-seed",
+  dataDirs: [
+    "/media/downloads/torrent/complete/prowlarr",
+    "/media/downloads/torrent/complete/movies",
+    "/media/downloads/torrent/complete/tv",
+  ],
+  maxDataDepth: 1,
+  outputDir: "/config/xseeds",
+  torrentDir: "/config/qBittorrent/BT_backup",
+};
